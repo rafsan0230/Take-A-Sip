5338eb90b1c1564e3a62f480e6de27673c8b8e91
rafsan0230 <rafsan0230@gmail.com>
2023-03-13 09:01:38 +0600

    data coming

--- server/index.js
+++ server/index.js
@@ -1,26 +1,28 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const router = require('./router');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const router = require("./router");
 const app = express();
-const PORT = 6789;
+
+// env variables
+require('dotenv').config();
+const PORT = process.env.PORT;
+const DB_URI = process.env.MONGOOSE_URI;
 
 const corsOptions = {
-  origin: 'http://localhost:4200',
+  origin: "http://localhost:4200",
   credentials: true,
-  exposedHeaders: "Authorization",
+  exposedHeaders: 'Authorization',
 };
 
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(router);
 
-
-(async function bootstrap () {
+(async function bootstrap() {
   try {
-    await mongoose.connect('mongodb+srv://afia:1234@cluster0.r0trdgv.mongodb.net/?retryWrites=true&w=majority');
+    await mongoose.connect(DB_URI);
     app.listen(PORT, () => console.log(`Server is listening on port ${PORT}.`));
   } catch (error) {
-    console.log(error);
   }
 })();
