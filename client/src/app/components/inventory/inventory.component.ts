20c83dcfe6bfca42a402147f1f5e7e28992fbf55
rafsan0230 <rafsan0230@gmail.com>
2023-03-15 10:49:07 +0600

    real time update on inventory

--- client/src/app/components/inventory/inventory.component.ts
+++ client/src/app/components/inventory/inventory.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inventory',
@@ -14,7 +15,7 @@ export class InventoryComponent {
 
 
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
   readonly inventoryURL = 'http://localhost:6789/inventory/getInventory';
   readonly addInventoryURL = 'http://localhost:6789/inventory';
 
@@ -25,14 +26,26 @@ export class InventoryComponent {
     this.addItem=true
   }
 
+  keyup(value : any) {
+    // this.inventoryDatas.re = value;
+  }
+
   submitQty(id:string, qty:any){
     console.log(id,qty)
     this.quantity=qty;
     this.http
         .put(this.addInventoryURL, {id,qty})
         .subscribe((response) => {
+          const getting = this.getInventoryDatas();
+          if (getting) {
+            getting.subscribe((response) => {
+              console.log(response);
+              this.inventoryDatas = response;
+            });
+          }
           console.log(response);
         });
+        // this.router.navigate(['inventory']);
   }
 
   ngOnInit() {
