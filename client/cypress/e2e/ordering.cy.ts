8731458c7853f427ce77f4b51f8a14019b4ac510
afahad0149 <fahad.akibahmed@gmail.com>
2023-03-16 11:39:00 +0600

    testing for ordering completed

--- client/cypress/e2e/ordering.cy.ts
+++ client/cypress/e2e/ordering.cy.ts
@@ -5,7 +5,7 @@ beforeEach(() => {
   cy.get('#submit-btn').click();
 });
 
-describe('starting e2e for Login', () => {
+describe('starting e2e for ordering', () => {
   it('User can place order', () => {
     cy.get('.card').eq(0).click();
     cy.contains('MILK').click();
@@ -13,13 +13,13 @@ describe('starting e2e for Login', () => {
     cy.contains('ADD TO LIST').click();
     cy.contains(' Order List').click();
     cy.get('.card-container').should('have.length', 1);
-    cy.wait(5000);
+    cy.get('.order-btn').click();
+    cy.wait(3000);
+    cy.url().should('contain', '/order');
+    cy.get('#room').type('Room 505');
+    cy.get('.select-section').select(1).invoke('val').should('eq', 'CLIENT');
+    cy.get('#submit-btn').click();
+    cy.get('#yes-btn').click();
+    cy.url().should('contain', '/home');
   });
-
-  it('should place order', () => {});
-
-  // it('Should select the card properly', () => {
-  //   cy.wait(5000);
-  //   cy.get('.card').click();
-  // });
 });
--- client/cypress/e2e/registration.cy.ts
+++ client/cypress/e2e/registration.cy.ts
@@ -6,15 +6,35 @@ describe('Starting e2e', () => {
     cy.title().should('equal', 'TakeASip');
   });
 
+  //success scenario
   it('Should register and add basic info form', () => {
     cy.get('#firstName').type('Nafiz');
     cy.get('#lastName').type('Rafsan');
     cy.get('#designation').type('Junior Software Engineer');
 
-    cy.get('#email').type('test110@gmail.com');
+    cy.get('#email').type('test101@gmail.com');
     cy.get('#password').type('1234567');
     cy.get('#confirmPassword').type('1234567');
     cy.get('#submit-btn').click();
-    // cy.visit('/home');
+    cy.url().should('contain', '/home');
+    cy.wait(2000);
+    cy.get('#logout').click();
+  });
+
+  // failure scenario
+  it('Should give error if the user is already registered', () => {
+    cy.get;
+    cy.get('#firstName').type('Nafiz');
+    cy.get('#lastName').type('Rafsan');
+    cy.get('#designation').type('Junior Software Engineer');
+
+    cy.get('#email').type('test101@gmail.com');
+    cy.get('#password').type('1234567');
+    cy.get('#confirmPassword').type('1234567');
+    cy.get('#submit-btn').click();
+    cy.get('.form-err-msg').should(
+      'contain',
+      'Account with given email exists.'
+    );
   });
 });
